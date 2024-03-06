8d36a35fefcbe05a9857cf06729e154e8a1fbedc
Mohammad Hossein Soleymani <hssoleymanmh@gmail.com>
2024-03-06 17:27:46 +0330

    fix: casecade to float in div

--- assignment1/src/main/java/edu/sharif/web/assignment1/Controllers/MainController.java
+++ assignment1/src/main/java/edu/sharif/web/assignment1/Controllers/MainController.java
@@ -2,11 +2,13 @@ package edu.sharif.web.assignment1.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calc")
 public class MainController {
-    @GetMapping("/calc/sum/{num1}/{num2}")
+    @GetMapping("/sum/{num1}/{num2}")
     public String add(@PathVariable("num1") String num1, @PathVariable("num2") String num2) {
         try {
             int operand1 = Integer.parseInt(num1);
@@ -18,7 +20,7 @@ public class MainController {
         }
     }
 
-    @GetMapping("/calc/sub/{num1}/{num2}")
+    @GetMapping("/subtract/{num1}/{num2}")
     public String subtract(@PathVariable("num1") String num1, @PathVariable("num2") String num2) {
         try {
             int operand1 = Integer.parseInt(num1);
@@ -31,7 +33,7 @@ public class MainController {
     }
 
 
-    @GetMapping("/calc/mul/{num1}/{num2}")
+    @GetMapping("/mult/{num1}/{num2}")
     public String multiply(@PathVariable("num1") String num1, @PathVariable("num2") String num2) {
         try {
             int operand1 = Integer.parseInt(num1);
@@ -43,13 +45,13 @@ public class MainController {
         }
     }
 
-    @GetMapping("/calc/div/{num1}/{num2}")
+    @GetMapping("/div/{num1}/{num2}")
     public String divide(@PathVariable("num1") String num1, @PathVariable("num2") String num2) {
         try {
             int operand1 = Integer.parseInt(num1);
             int operand2 = Integer.parseInt(num2);
-            int result = operand1 / operand2;
-            return String.format("%d / %d = %d", operand1, operand2, result);
+            float result = (float) operand1 / operand2;
+            return String.format("%d / %d = %f", operand1, operand2, result);
         } catch (NumberFormatException | ArithmeticException e) {
             return "Invalid URL";
         }
